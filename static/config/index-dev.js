91f0e7da24e19ba6e815aade14662ad0f60a201f
Leeyulong <1097910340@qq.com>
2020-06-15 21:24:47 +0800

    udpate

--- static/config/index-dev.js
+++ static/config/index-dev.js
@@ -1,9 +1,9 @@
 /**
  * 开发环境
  */
-; (function () {
-    window.SITE_CONFIG = {}
+(function() {
+    window.SITE_CONFIG = {};
     // api接口请求地址
-    window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/myblog'
+    window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/lylblog';
     // window.SITE_CONFIG['baseUrl'] = 'http://182.254.171.61:8080/dbblog/'
-})()
+})();
