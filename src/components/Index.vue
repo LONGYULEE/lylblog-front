585be3866433c719879ade237f81f152d95acf38
Longyulee <1097910340@qq.com>
2020-11-09 23:17:14 +0800

    首页壁纸

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="main-wrapper">
+        <div id="test"></div>
         <router-view name="header"></router-view>
         <router-view name="content"></router-view>
         <router-view name="footer"></router-view>
@@ -28,6 +29,13 @@ export default {
     width: 100%;
     margin: 1px 0 auto;
     padding-top: 35px;
+
+    #test {
+        width: 100%;
+        height: 100vh;
+        margin: -96px 0 30px 0px;
+        background: rgba(0, 0, 0, 0.2);
+    }
 
     @media only screen and (max-width: 992px) {
         background-color: white;
